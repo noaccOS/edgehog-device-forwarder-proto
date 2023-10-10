1b6056dfa3eed3a6b99d89a1ebb2e3698d4c9376
Francesco Noacco <francesco.noacco@secomind.com>
2023-10-10 15:37:43 +0200

    feat(check-diff): POSIX compliance
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- scripts/check-diff.sh
+++ scripts/check-diff.sh
@@ -1,13 +1,14 @@
-#! /usr/bin/env bash
+#!/bin/sh
 
 # Copyright 2023 SECO Mind Srl
 # SPDX-License-Identifier: Apache-2.0
 
-set -exEuo pipefail
+set -exu
 
 DIFF=$(git diff --exit-code)
 
-if [[ "$?" != 0 ]] ; then
+if [ $? -ne 0 ]
+then
     echo "differences between the generated files" >&2
     echo "$DIFF"
     exit 1
